4bca64c031fdb74d6be9358d082241e8be9ef5b4
Weellyyy <wildanprtm2304@gmail.com>
2024-03-07 19:13:26 +0700

    menambahkan fungsi bubble sort

--- Praktikum1/Praktikum1.cpp
+++ Praktikum1/Praktikum1.cpp
@@ -13,6 +13,7 @@ void input() {
         else {
             cout << "\nArray hanya dapat menampung 20 elemen\n";
         }
+    }
         cout << endl;
         cout << "=================\n";
         cout << "Masukkan Elemen Array\n";
@@ -23,6 +24,24 @@ void input() {
             cin >> arr[i];
         }
 
+}
+
+    void bubbleSortArray() {
+        int pass = 1;
+        do {
+            for (int j = 0; j <= n - 1 - pass; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            pass = pass + 1;
+
+            cout << "\nPass " << pass - 2 << ": ";
+            for (int k = 0; k < n; k++) {
+                cout << arr[k] << "";
+            }
+            cout << endl;
+        } while (pass <= n - 1);
     }
-
-
