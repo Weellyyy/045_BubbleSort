cf73f26387d49e367d8c10a19b009ce3539dbfc2
Weellyyy <wildanprtm2304@gmail.com>
2024-03-07 18:58:41 +0700

    menambahkan fungsi input

--- Praktikum1/Praktikum1.cpp
+++ Praktikum1/Praktikum1.cpp
@@ -3,4 +3,26 @@ using namespace std;
 int a[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan Banyak elemen Pada Array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
 
+        else {
+            cout << "\nArray hanya dapat menampung 20 elemen\n";
+        }
+        cout << endl;
+        cout << "=================\n";
+        cout << "Masukkan Elemen Array\n";
+        cout << "================\n";
+
+        for (int i = 0; i < n; i++) {
+            cout << "Data ke-" << (i + 1) << ": ";
+            cin >> arr[i];
+        }
+
+    }
+
+}
